4443f23ae90a63d0590bca90f14a7c3e8b6e1e37
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-05-18 19:14:09 -1000

    Done, played with a few different things.

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,17 @@
 'use strict';
-module.exports = function() {
-  
-};
+// module.exports = function() {
+//   var secretNumber=Math.floor((Math.random() * 1000000) + 1);
+//   return function(){
+//     return secretNumber;
+// }
+// };
+
+module.exports= function(){
+
+  var rando=Math.floor((Math.random() * 1000000) + 1)
+  var newNumber= rando;
+  return function(){
+    return newNumber;
+    }
+
+  }
